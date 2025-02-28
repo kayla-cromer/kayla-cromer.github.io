6756a8c3e196e7a5114d4e7b0570b29e5531b903
kayla-cromer <articsirene@gmail.com>
2025-02-28 01:24:28 -0500

    more files made / updated

---
+++ submit_form.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Here you can add code to send the email or save the data to a database
+    // For example, using the mail function to send an email
+    $to = "your-email@example.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for contacting us, $name. We will get back to you shortly.";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
